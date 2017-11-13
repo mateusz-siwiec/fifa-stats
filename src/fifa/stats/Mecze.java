76c7280b1e3d59d90c0e79dc597cf97c713f1c1f
Mateusz Siwiec <mateuszsiwiec3@gmail.com>
2017-11-13 15:43:10 +0100

    Dodano klasy i pola klas (jak w bazie danych)

---
+++ src/fifa/stats/Mecze.java
@@ -0,0 +1,14 @@
+
+package fifa.stats;
+import java.util.Date;
+
+public class Mecze {
+    private int id;
+    private int gospodarzId;
+    private int gospodarzBramki;
+    private int gospodarzDruzynaId;
+    private int goscId;
+    private int goscBramki;
+    private int goscDruzynaId;
+    private Date czasRozegrania;
+}

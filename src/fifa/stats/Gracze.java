217867dac5fb379eeaa28587e141057d85ef3700
Mateusz Siwiec <mateuszsiwiec3@gmail.com>
2017-11-13 16:04:44 +0100

    Get and set

--- src/fifa/stats/Gracze.java
+++ src/fifa/stats/Gracze.java
@@ -1,18 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package fifa.stats;
 
-/**
- *
- * @author Mateusz
- */
+
 public class Gracze {
     private int id;
     private String imie;
     private String nazwisko;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
     
     
 }

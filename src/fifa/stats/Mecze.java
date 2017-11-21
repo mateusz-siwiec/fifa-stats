e4324dd0b39d34922f47876041a82adef6da9a8a
Mateusz Siwiec <mateuszsiwiec3@gmail.com>
2017-11-21 13:18:01 +0100

    #7

--- src/fifa/stats/Mecze.java
+++ src/fifa/stats/Mecze.java
@@ -5,10 +5,10 @@ import java.util.Date;
 
 public class Mecze {
     private int id;
-    private int wynikGospodarza;
-    private int wynikGoscia;
+    private WynikGracza wynikGospodarza;
+    private WynikGracza wynikGoscia;
     private Date czasRozegrania;
-    
+
     public int getId() {
         return id;
     }
@@ -17,7 +17,21 @@ public class Mecze {
         this.id = id;
     }
 
-    
+    public WynikGracza getWynikGospodarza() {
+        return wynikGospodarza;
+    }
+
+    public void setWynikGospodarza(WynikGracza wynikGospodarza) {
+        this.wynikGospodarza = wynikGospodarza;
+    }
+
+    public WynikGracza getWynikGoscia() {
+        return wynikGoscia;
+    }
+
+    public void setWynikGoscia(WynikGracza wynikGoscia) {
+        this.wynikGoscia = wynikGoscia;
+    }
 
     public Date getCzasRozegrania() {
         return czasRozegrania;
@@ -27,27 +41,13 @@ public class Mecze {
         this.czasRozegrania = czasRozegrania;
     }
 
-    public int getWynikGospodarza() {
-        return wynikGospodarza;
-    }
-
-    public void setWynikGospodarza(int wynikGospodarza) {
-        this.wynikGospodarza = wynikGospodarza;
-    }
-
-    public int getWynikGoscia() {
-        return wynikGoscia;
-    }
-
-    public void setWynikGoscia(int wynikGoscia) {
-        this.wynikGoscia = wynikGoscia;
-    }
-
     @Override
     public String toString() {
         return "Mecze{" + "id=" + id + ", wynikGospodarza=" + wynikGospodarza + ", wynikGoscia=" + wynikGoscia + ", czasRozegrania=" + czasRozegrania + '}';
     }
-
+    
+  
+    
     
     
     

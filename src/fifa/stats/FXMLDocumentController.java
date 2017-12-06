52bddd0950e297033a2bd795112ef66156220cd4
Mateusz Siwiec <mateuszsiwiec3@gmail.com>
2017-12-06 12:10:08 +0100

    Nowy layout

--- src/fifa/stats/FXMLDocumentController.java
+++ src/fifa/stats/FXMLDocumentController.java
@@ -10,12 +10,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
+    
     @FXML 
     private TextField tfGospodarzImie;
     @FXML
@@ -33,6 +35,19 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private TextField tfGoscBramki;
     
+    @FXML
+    private ImageView btn_home;
+     @FXML
+    private ImageView btn_user;
+     @FXML
+    private ImageView btn_results;
+     @FXML
+     private AnchorPane h_home;
+      @FXML
+     private AnchorPane h_user;
+       @FXML
+     private AnchorPane h_results;
+    
     
     @FXML
     private void dodajWynik(ActionEvent event) {
@@ -43,6 +58,25 @@ public class FXMLDocumentController implements Initializable {
         
        
    
+    }
+    @FXML
+    private void handleButtonAction(MouseEvent event){
+        if(event.getTarget()==btn_home){
+            h_home.setVisible(true);
+             h_user.setVisible(false);
+             h_results.setVisible(false);
+        }
+        else if(event.getTarget()==btn_user){
+            h_user.setVisible(true);
+            h_home.setVisible(false);
+            h_results.setVisible(false);
+        }
+        else if(event.getTarget()==btn_results){
+            h_results.setVisible(true);
+            h_user.setVisible(false);
+            h_home.setVisible(false);
+        }
+             
     }
     
     @Override
